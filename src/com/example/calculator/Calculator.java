f650b894bf2aa97b6c216b77e12f61c09fe3cd62
minss22 <kms141516@gmail.com>
2025-10-15 12:54:50 +0900

    예외 처리

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] symbols = {"+", "-", "*", "/"};
-        double result = 0;
 
         try {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -15,26 +13,25 @@ public class Calculator {
             System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
             String symbol = sc.next();
 
-            switch (symbol) {
-                case "+":
-                    result = n1 + n2;
-                    break;
-                case "-":
-                    result = n1 - n2;
-                    break;
-                case "*":
-                    result = n1 * n2;
-                    break;
-                case "/":
-                    if (n2 == 0) {
-                        throw new RuntimeException();
-                    }
-                    result = n1 / (double) n2;
+            if (n1<0 || n2<0){
+                throw new RuntimeException("⚠️양의 정수를 입력해주세요.");
             }
+            double result = switch (symbol) { // 계산 결과
+                case "+" -> n1 + n2;
+                case "-" -> n1 - n2;
+                case "*" -> n1 * n2;
+                case "/" -> {
+                    if (n2 == 0) {
+                        throw new RuntimeException("️⚠️나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    }
+                    yield n1 / (double) n2;
+                }
+                default -> throw new RuntimeException("️⚠️사칙연산 기호를 잘못 입력했습니다.");
+            };
 
-            System.out.printf("사칙연산의 결과: %d %s %d = %s", n1, symbol, n2, result);
+            System.out.printf("결과: %d %s %d = %s", n1, symbol, n2, result);
         } catch (RuntimeException e) {
-            System.out.println("⚠️나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            System.out.println(e.getMessage());
         }
 
     }

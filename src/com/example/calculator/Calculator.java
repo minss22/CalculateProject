062a0a29bc64df126fa079258e0970b748380b8a
minss22 <kms141516@gmail.com>
2025-10-15 14:33:09 +0900

    반복 처리

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -5,34 +5,39 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        try {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int n1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int n2 = sc.nextInt();
-            System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
-            String symbol = sc.next();
+        String answer;
+        do {
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int n1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int n2 = sc.nextInt();
+                System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
+                String symbol = sc.next();
 
-            if (n1<0 || n2<0){
-                throw new RuntimeException("⚠️양의 정수를 입력해주세요.");
-            }
-            double result = switch (symbol) { // 계산 결과
-                case "+" -> n1 + n2;
-                case "-" -> n1 - n2;
-                case "*" -> n1 * n2;
-                case "/" -> {
-                    if (n2 == 0) {
-                        throw new RuntimeException("️⚠️나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    }
-                    yield n1 / (double) n2;
+                if (n1 < 0 || n2 < 0) {
+                    throw new RuntimeException("⚠️양의 정수를 입력해주세요.");
                 }
-                default -> throw new RuntimeException("️⚠️사칙연산 기호를 잘못 입력했습니다.");
-            };
+                double result = switch (symbol) { // 계산 결과
+                    case "+" -> n1 + n2;
+                    case "-" -> n1 - n2;
+                    case "*" -> n1 * n2;
+                    case "/" -> {
+                        if (n2 == 0) {
+                            throw new RuntimeException("️⚠️나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        }
+                        yield n1 / (double) n2;
+                    }
+                    default -> throw new RuntimeException("️⚠️사칙연산 기호를 잘못 입력했습니다.");
+                };
 
-            System.out.printf("결과: %d %s %d = %s", n1, symbol, n2, result);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-
+                System.out.printf("결과: %d %s %d = %s", n1, symbol, n2, result);
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("\n\n더 계산하려면 아무 키나 입력해주세요. (exit 입력 시 종료)");
+            sc.nextLine(); // 개행문자 제거
+            answer = sc.nextLine(); // 개행문자도 인식
+        } while (!answer.equals("exit"));
     }
 }

f900dbc26dbbe52fe9787e1a1f4fd73643ed8910
minss22 <kms141516@gmail.com>
2025-10-15 12:32:56 +0900

    연산 오류 처리

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -7,27 +7,35 @@ public class Calculator {
         String[] symbols = {"+", "-", "*", "/"};
         double result = 0;
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int n1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int n2 = sc.nextInt();
-        System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
-        String symbol = sc.next();
+        try {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int n1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int n2 = sc.nextInt();
+            System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
+            String symbol = sc.next();
 
-        switch (symbol) {
-            case "+":
-                result = n1 + n2;
-                break;
-            case "-":
-                result = n1 - n2;
-                break;
-            case "*":
-                result = n1 * n2;
-                break;
-            case "/":
-                result = n1 / (double) n2;
+            switch (symbol) {
+                case "+":
+                    result = n1 + n2;
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    break;
+                case "/":
+                    if (n2 == 0) {
+                        throw new RuntimeException();
+                    }
+                    result = n1 / (double) n2;
+            }
+
+            System.out.printf("사칙연산의 결과: %d %s %d = %s", n1, symbol, n2, result);
+        } catch (RuntimeException e) {
+            System.out.println("⚠️나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
         }
 
-        System.out.printf("사칙연산의 결과: %d %s %d = %s", n1, symbol, n2, result);
     }
 }

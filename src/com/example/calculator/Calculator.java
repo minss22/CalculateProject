21a9543aa9260c0d390453a3573bacc2577d1bd1
minss22 <kms141516@gmail.com>
2025-10-15 12:21:34 +0900

    입력 및 계산

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,4 +1,33 @@
 package com.example.calculator;
+import java.util.Scanner;
 
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] symbols = {"+", "-", "*", "/"};
+        double result = 0;
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int n1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int n2 = sc.nextInt();
+        System.out.print("사칙연산 기호(+, -, *, /)를 입력하세요: ");
+        String symbol = sc.next();
+
+        switch (symbol) {
+            case "+":
+                result = n1 + n2;
+                break;
+            case "-":
+                result = n1 - n2;
+                break;
+            case "*":
+                result = n1 * n2;
+                break;
+            case "/":
+                result = n1 / (double) n2;
+        }
+
+        System.out.printf("사칙연산의 결과: %d %s %d = %s", n1, symbol, n2, result);
+    }
 }

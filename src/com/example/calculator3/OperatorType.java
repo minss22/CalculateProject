aacfc070893aab93c6f5b90bbb360cfe3d291991
minss22 <kms141516@gmail.com>
2025-10-20 16:05:35 +0900

    Enum에 람다식 활용

--- src/com/example/calculator3/OperatorType.java
+++ src/com/example/calculator3/OperatorType.java
@@ -1,18 +1,25 @@
 package com.example.calculator3;
+import java.util.function.DoubleBinaryOperator;
 
 public enum OperatorType {
-    PLUS("+"),
-    MINUS("-"),
-    MULTIPLY("*"),
-    DIVIDE("/");
+    PLUS("+", (x, y) -> x + y),
+    MINUS("-", (x, y) -> x - y),
+    MULTIPLY("*", (x, y) -> x * y),
+    DIVIDE("/", (x, y) -> x / y);
 
-    private final String symbol;
+    private final String symbol; // 사칙연산 기호
+    private final DoubleBinaryOperator op; // 람다식 저장
 
-    OperatorType(String symbol) {
+    OperatorType(String symbol, DoubleBinaryOperator op) {
         this.symbol = symbol;
+        this.op = op;
     }
 
     public String getSymbol() {
         return symbol;
+    }
+
+    public double apply(double x, double y) {
+        return op.applyAsDouble(x, y);
     }
 }
